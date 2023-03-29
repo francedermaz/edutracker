9f922fd1236b69f5b2d667455d50b75dd2e23008
francedermaz <francedermaz@gmail.com>
2023-03-29 12:04:31 -0300

    Refactor routes to be dev friendly

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const authRouter = require('./auth.js');
+const roomRouter = require('./room.js');
+const studentsRouter = require('./students.js');
 
 const router = Router();
 
 router.use('/', authRouter);
+router.use('/rooms', roomRouter);
+router.use('/students', studentsRouter);
 
 module.exports = router;
