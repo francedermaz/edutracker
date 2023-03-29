99023158743181114b903a9ca8301788445ca37e
francedermaz <francedermaz@gmail.com>
2023-03-29 10:43:03 -0300

    Add relations. Cleanup

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,8 @@
 const { Router } = require('express');
-// Importar todos los routers;
-
 const authRouter = require('./auth.js');
 
 const router = Router();
 
-// Configurar los routers
-
 router.use('/auth', authRouter);
-
 
 module.exports = router;
